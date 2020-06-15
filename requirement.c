0f62b6247f18d0535620c2d1e1f4607c6d510d30
mgl <marius@mgl.dev>
2020-06-15 09:26:05 +0200

    handle overflow in factrec

--- requirement.c
+++ requirement.c
@@ -5,11 +5,13 @@
 ** my_factrec_synhesis
 */
 
+#include <stdio.h>
+
 int my_factrec_synthesis(int nb)
 {
     int res = 1;
 
-    if (nb < 0 || nb > 16)
+    if (nb < 0 || nb > 12)
         return (0);
     if (nb == 0)
         return (1);
@@ -34,4 +36,11 @@ int my_squareroot_synthesis(int nb)
         return (i);
     else
         return (-1);
+}
+
+int main(void)
+{
+    for (int i = -5; i < 16; i++)
+        printf("%d! = %d\n", i, my_factrec_synthesis(i));
+    return (0);
 }
