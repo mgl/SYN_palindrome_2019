7923cfa6abde8b1829cdba085f450fd6a9e2892e
mgl <marius@mgl.dev>
2020-06-15 09:18:13 +0200

    requirement.c

---
+++ requirement.c
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2020
+** ADM_palindrome_2019
+** File description:
+** my_factrec_synhesis
+*/
+
+int my_factrec_synthesis(int nb)
+{
+    int res = 1;
+
+    if (nb < 0 || nb > 16)
+        return (0);
+    if (nb == 0)
+        return (1);
+    for (int i = 1; i <= nb; i++)
+        res *= i;
+    return (res);
+}
+
+int my_squareroot_synthesis(int nb)
+{
+    int res = 1;
+    int i = 0;
+
+    if (nb < 0)
+        return (0);
+    if (nb == 1 || nb == 0)
+        return (nb);
+    for (; res < nb; i++)
+        res = i * i;
+    i -= 1;
+    if (i * i == nb)
+        return (i);
+    else
+        return (-1);
+}
